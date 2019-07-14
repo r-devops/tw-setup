22dee8b9a9f22498c16e4e14078aebab790c793e
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 09:48:19 +0530

    Checking Variables in Jenkins

--- jenkins/job.groovy
+++ jenkins/job.groovy
@@ -1,7 +1,14 @@
 node() {
-    if(typeof(PROJ_NAME)=="undefined"){   
-        println "Hello PROJ_NAME is not there"   
+
+    def check_vars = ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"]
+    for (item in check_vars) {
+        try {
+            println item
+        } catch(e) {
+            println "Variable " item "missing"
+        }
     }
+
     stage('Infra Setup - Terraform') {
         sh '''
             cd terraform
