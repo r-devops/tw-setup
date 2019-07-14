f5e21779cdf620592c18204449639e2724c9bb24
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 12:26:07 +0530

    Update Subnet Group

--- terraform/modules/rds/groups.tf
+++ terraform/modules/rds/groups.tf
@@ -1,7 +1,7 @@
 ## Subnet Group using Private Subnets
 resource "aws_db_subnet_group" "mariadb" {
     name                        = "mariadb"
-    subnet_ids                  = ["${var.PRIVATE_SUBNETS}"]
+    subnet_ids                  = "${var.PRIVATE_SUBNETS}"
 
     tags                        = {
         Name                    = "${var.PROJECT_NAME}-RDS-SubnetGroup"
