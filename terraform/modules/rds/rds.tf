79af506701b137c183eb3fdaf22dd31fdce034c0
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 12:42:01 +0530

    Add RDS Instance

---
+++ terraform/modules/rds/rds.tf
@@ -0,0 +1,18 @@
+resource "aws_db_instance" "mariadb-rds" {
+    identifier                              = "wiki-rds"
+    allocated_storage                       = 20
+    storage_type                            = "gp2"
+    engine                                  = "mariadb"
+    engine_version                          = "10.3"
+    instance_class                          = "db.t2.micro"
+    name                                    = "wiki"
+    username                                = "${var.DBUSER}"
+    password                                = "${var.DBPASS}"
+    parameter_group_name                    = "${aws_db_parameter_group.mariadb.id}"
+    db_subnet_group_name                    = "${aws_db_subnet_group.mariadb.id}"
+    vpc_security_group_ids                  = "${aws_security_group.rds-sg.id}"
+    skip_final_snapshot                     = true ## Optionally enabled for deleting through terraform
+    tags                                    = {
+        Name                                = "${var.PROJECT_NAME}-rds"
+    }
+}
