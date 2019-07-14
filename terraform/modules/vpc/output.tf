0c79bf7df9c56bc9f3ae2f5e428adb713a425fa5
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 12:15:02 +0530

    Added outputs to VPC module

---
+++ terraform/modules/vpc/output.tf
@@ -0,0 +1,11 @@
+output "PRIVATE_SUBNETS" {
+    value = "${aws_subnet.private-subnets.*.id}"
+}
+
+output "PUBLIC_SUBNETS" {
+    value = "${aws_subnet.public-subnets.*.id}"
+}
+
+output "VPC_ID" {
+    value = "${aws_vpc.main.id}"
+}
