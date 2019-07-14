959d117db9b9f991ddbfe84e2f32664468de1ac1
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 10:48:14 +0530

    Checking Variables in Jenkins

--- jenkins/job.groovy
+++ jenkins/job.groovy
@@ -3,12 +3,11 @@ node() {
     def check_vars = ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"]
     for (item in check_vars) {
         try {
-            //def x = "${item}"
-            def script = "Hello ${item}"
-            def z = Eval.me(script + '".toString()')
-            println z
+            GroovyShell shell = new GroovyShell();
+            Closure c = shell.evaluate("""{->"$item"}""")
+            c.call()
         } catch(e) {
-            println "Variable " item "missing"
+            println "Variable " item "is missing"
             System.exit(1)
         }
     }
