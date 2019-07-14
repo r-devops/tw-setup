cf60cc84b24152f2ef3feab9a00426143cf798df
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 11:48:23 +0530

    Update Terraform Variables correction of names

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
     cidr_block          =   "${var.VPC_CIDR}"
     instance_tenancy    =   "default"
-    tags {
+    tags                = {
         Name            = "${var.PROJECT_NAME}-vpc"
     }
 }
