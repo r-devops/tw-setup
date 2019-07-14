67cfd0e3e94c6f6ee5ad59785c497f57983f14f8
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 09:25:42 +0530

    Checking Variables in Jenkins

--- jenkins/job.groovy
+++ jenkins/job.groovy
@@ -1,7 +1,12 @@
 node() {
+    if(typeof(PROJ_NAME1)=="undefined"){   
+        println "Hello PROJ_NAME is not there"   
+    }
     stage('Infra Setup - Terraform') {
         sh '''
-            env
+            cd terraform
+            terraform init
+            terraform plan
         '''
     }
 }
