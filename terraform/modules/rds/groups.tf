4a7a994859ca7204342bf98d9ff0ef34c4409831
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 12:32:22 +0530

    Add security group

--- terraform/modules/rds/groups.tf
+++ terraform/modules/rds/groups.tf
@@ -13,3 +13,25 @@ resource "aws_db_parameter_group" "mariadb" {
     name                        = "mariadb-rds"
     family                      = "mariadb10.3"
 }
+
+## Create Security Group for MariaDB access , Only inside VPC
+
+resource "aws_security_group" "rds-sg" {
+    name                        = "allow-mariadb-internal"
+    description                 = "Allow MariaDB Access Internal"
+    vpc_id                      = "${var.VPC_ID}"
+
+    ingress {
+      from_port                 = 443
+      to_port                   = 443
+      protocol                  = "-1"
+      cidr_blocks               = "${var.VPC_CIDR}"
+    }
+
+    egress {
+      from_port                 = 0
+      to_port                   = 0
+      protocol                  = "-1"
+      cidr_blocks               = ["0.0.0.0/0"]
+    }
+}
