c55d3ad96fb32dbda7ff2b6533093b0a2f59cf29
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 10:56:28 +0530

    Update Jenkins Image

---
+++ docker/images/jenkins/authorize.groovy
@@ -0,0 +1,11 @@
+import jenkins.model.Jenkins
+import jenkins.security.QueueItemAuthenticatorConfiguration
+import hudson.model.*
+import org.jenkinsci.plugins.authorizeproject.GlobalQueueItemAuthenticator
+import org.jenkinsci.plugins.authorizeproject.strategy.TriggeringUsersAuthorizationStrategy
+
+println("=== Configure Authorize Project")
+GlobalQueueItemAuthenticator auth = new GlobalQueueItemAuthenticator(
+    new TriggeringUsersAuthorizationStrategy()
+)
+QueueItemAuthenticatorConfiguration.get().authenticators.add(auth)
