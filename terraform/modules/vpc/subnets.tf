61db8c972181d77d35742ba6645c8ccacdcea157
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 11:51:53 +0530

    Update Terraform Variables correction of names

--- terraform/modules/vpc/subnets.tf
+++ terraform/modules/vpc/subnets.tf
@@ -9,3 +9,15 @@ resource "aws_subnet" "public-subnets" {
         Name                    = "${var.PROJECT_NAME}-Public-Subnet-${count.index+1}"
   }
 }
+
+resource "aws_subnet" "public-subnets" {
+    count                       = "${length(data.aws_availability_zones.available)}"
+    vpc_id                      = "${aws_vpc.main.id}"
+    cidr_block                  = "${cidrsubnet(var.VPC_CIDR, 8, count.index)}"
+    availability_zone           = "${element(data.aws_availability_zones.available.names, count.index+length(data.aws_availability_zones.available))}"
+    map_public_ip_on_launch     = true
+
+    tags = {
+        Name                    = "${var.PROJECT_NAME}-Public-Subnet-${count.index+1}"
+  }
+}
