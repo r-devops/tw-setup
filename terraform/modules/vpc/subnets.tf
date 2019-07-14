ebed07f234016098f0b9bb5d2ba0e2e416e0762a
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 12:07:58 +0530

    Update of Subnets

--- terraform/modules/vpc/subnets.tf
+++ terraform/modules/vpc/subnets.tf
@@ -1,3 +1,5 @@
+
+#Create Public Subnets
 resource "aws_subnet" "public-subnets" {
     count                       = "${length(data.aws_availability_zones.available)}"
     vpc_id                      = "${aws_vpc.main.id}"
@@ -10,6 +12,7 @@ resource "aws_subnet" "public-subnets" {
   }
 }
 
+# Create Private Subnets
 resource "aws_subnet" "private-subnets" {
     count                       = "${length(data.aws_availability_zones.available)}"
     vpc_id                      = "${aws_vpc.main.id}"
@@ -20,4 +23,41 @@ resource "aws_subnet" "private-subnets" {
     tags = {
         Name                    = "${var.PROJECT_NAME}-Private-Subnet-${count.index+1}"
   }
+}
+
+# Create Route  for Public Subnets
+resource "aws_route_table" "public-rt" {
+    vpc_id                      = "${aws_vpc.default.id}"
+
+    route {
+      cidr_block                = "0.0.0.0/0"
+      gateway_id                = "${aws_internet_gateway.gw.id}"
+    }
+
+    tags                        = {
+        Name                    = "${var.PROJECT_NAME}-Public-RT"
+    }
+}
+
+# Create Route  for Private Subnets
+resource "aws_route_table" "private-rt" {
+    vpc_id                      = "${aws_vpc.default.id}"
+
+    tags                        = {
+        Name                    = "${var.PROJECT_NAME}-Private-RT"
+    }
+}
+
+## Associate Public-Route table to Public Subnet
+resource "aws_route_table_association" "public-assoc" {
+    count                       = "${length(data.aws_availability_zones.available)}"
+    subnet_id                   = "${element(aws_subnet.public-subnets.*.id,count.index)}"
+    route_table_id              = "${aws_route_table.public-rt.id}"
+}
+
+## Associate Private-Route table to Private Subnet
+resource "aws_route_table_association" "private-assoc" {
+    count                       = "${length(data.aws_availability_zones.available)}"
+    subnet_id                   = "${element(aws_subnet.private-subnets.*.id,count.index)}"
+    route_table_id              = "${aws_route_table.private-rt.id}"
 }
--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,7 +1,16 @@
+# Create A VPC 
 resource "aws_vpc" "main" {
     cidr_block          =   "${var.VPC_CIDR}"
     instance_tenancy    =   "default"
     tags                = {
         Name            = "${var.PROJECT_NAME}-vpc"
     }
+}
+
+# Create IGW for Public Access 
+resource "aws_internet_gateway" "gw" {
+    vpc_id              = "${aws_vpc.main.id}"
+    tags                = {
+        Name            = "${var.PROJECT_NAME}-igw"
+  }
 }
