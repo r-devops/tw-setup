9ac59b27725ef7dc59f96a4ac8c6377b0a35581a
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-14 12:28:51 +0530

    Create Parameter Group for RDS

--- terraform/modules/rds/groups.tf
+++ terraform/modules/rds/groups.tf
@@ -7,3 +7,9 @@ resource "aws_db_subnet_group" "mariadb" {
         Name                    = "${var.PROJECT_NAME}-RDS-SubnetGroup"
   }
 }
+
+## Create Parameter Group for MariaDB
+resource "aws_db_parameter_group" "mariadb" {
+    name                        = "mariadb-rds"
+    family                      = "mariadb10.3"
+}
