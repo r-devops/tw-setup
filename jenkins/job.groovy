6d5cbb7ca0357543ee42080d009b6098d30035fc
Sriharsha Kalluru <harsha2006@gmail.com>
2019-07-16 12:27:19 +0530

    Update of Jenkins Jobs

--- jenkins/job.groovy
+++ jenkins/job.groovy
@@ -48,7 +48,7 @@ node() {
             sh '''
                 cd terraform 
                 terraform init
-                touch modules/ec2/deployer
+                touch deployer
                 terraform destroy -target=module.EC2 -auto-approve -var-file=proj-info.tfvars -var DBUSER=${RDS_USERNAME} -var DBPASS=${RDS_PASSWORD}
                 terraform apply -auto-approve -var-file=proj-info.tfvars -var DBUSER=${RDS_USERNAME} -var DBPASS=${RDS_PASSWORD}
             '''
